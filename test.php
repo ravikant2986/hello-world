a47f66c6f2cdfcd06b88d78ab9ab37b27b929747
ravikant2986 <ravikant@nascenture.com>
2018-01-20 13:06:24 +0530

    updated test fil rre

--- test.php
+++ test.php
@@ -1,13 +1,5 @@
 <?php 
 
- 
-if (isset($_SERVER['HTTP_ORIGIN'])) {
-    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
-    header('Access-Control-Allow-Credentials: true');
-    header('Access-Control-Max-Age: 86400');    // cache for 1 day
-}
-
-
 mysql_connect('localhost','root','root');
 mysql_select_db('booing_engine');
      
